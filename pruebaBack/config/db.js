257e58b2bcde425e0a27cc90e4c4b23c18ba692c
jfperez <jfperez9510@gmail.com>
2025-05-03 12:41:58 -0500

    Comentario de prueba

--- pruebaBack/config/db.js
+++ pruebaBack/config/db.js
@@ -15,7 +15,7 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
-//Comentario de prueba
+//Comentario de prueba para subir
 const db = new Sequelize(URI, {
   dialect: 'postgres',
   logging: false
